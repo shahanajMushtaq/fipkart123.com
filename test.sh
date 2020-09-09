aeed4bb853160e6e352fd73741dfd1de92663d18
Mushtaq Ahmed <muahmed1980@gmail.com>
2020-09-09 14:34:11 +0000

    test.sh updated

--- test.sh
+++ test.sh
@@ -1,2 +1,5 @@
-echo "Hello"
-echo "Welcome to Filpkart"
+v_pass=`cat password`
+echo "connect to db.."
+mysql -h dbhost.filpkart.com -u dbuser -p $v_pass dev-filpkart <<EOF
+'select * from employee;'
+EOF
